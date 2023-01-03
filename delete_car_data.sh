d6be3258e0d7f013bc473f2036b9937ca0b78560
mathewacarter29 <mathewacarter29@vt.edu>
2023-01-03 17:43:33 -0500

    Got Carvana data in JSON format

---
+++ delete_car_data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -vrf car_data/* | echo "$(wc -l) files deleted"
--- pull_car_data.py
+++ pull_car_data.py
@@ -1,19 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import sys
-
-now = str(datetime.now())
-filename = now.replace(' ', '_') + '_carvana.txt'
-filename = filename.replace(':', ';')
-sys.stdout = open(filename, 'w')
-
+import json
+import os
 
 url = "https://www.carvana.com/cars/suv"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-
 
 results = soup.find_all(attrs={'class': 'result-tile'})
 
@@ -22,18 +16,46 @@ results = soup.find_all(attrs={'class': 'result-tile'})
 #   print(div.prettify())
 
 # going by the data-qa attribute every field element has
-# fuck shipping cost?
 # mileage has other crap in it - split after weird character
-fields = ['make-model', 'trim-mileage', 'price', 'monthly-payment', 'shipping-cost', 'get-it-by']
-fields_readable= ['Car Name', 'Mileage', 'Price', 'Monthly Payment', 'Shipping Cost', 'Delivery']
+fields = ['make-model', 'trim-mileage', 'price', 'monthly-payment', 'get-it-by']
+data = {}
 
 for result in results:
+  car_id = result.find('a').get('href')
+
+  car_data = {'make-model': '', 'mileage': '', 'price': '', 'monthly-payment': '', 'get-it-by': ''}
+  
   for index, field in enumerate(fields):
     curr_field_value = result.find(attrs={'data-qa': field})
+    field_text = curr_field_value.get_text()
+    if field == 'trim-mileage':
 
-    print(fields_readable[index] + ':', curr_field_value.get_text())
+      # the mileage part of a result tile also has info on the type of car,
+      # so we have to split the string by the middle dot character and append
+      # the car data to the make-model
+      arr = field_text.split(' • ')
+      car_data['make-model'] += ' - ' + arr[0]
+      car_data['mileage'] = arr[1]
 
-  print()
+    elif field == 'get-it-by':
+      car_data[field] = field_text.replace('Get it by ', '')
 
-# print(results[0].prettify())
+    else:
+      car_data[field] = field_text
 
+  data[car_id] = car_data
+
+json_data = json.dumps(data, indent = 4)
+
+# if the car_data folder does not exist, create it
+folder_name = 'car_data/'
+if not os.path.exists(folder_name):
+  os.mkdir(folder_name)
+
+now = str(datetime.now())
+filename = now.replace(' ', '_') + '_carvana.json'
+filename = 'car_data/' + filename.replace(':', ';')
+with open(filename, 'w') as output_file:
+  output_file.write(json_data)
+
+print('Data written to:', filename)
