f9c3a9bf1668672c61b0592af3880025d8bcd9c8
mathewacarter29 <mathewacarter29@vt.edu>
2023-01-03 20:51:01 -0500

    Pulls data on multiple pages worth of cars

--- pull_car_data.py
+++ pull_car_data.py
@@ -1,61 +1,77 @@
+#!/usr/bin/env python
+
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import json
 import os
 
-url = "https://www.carvana.com/cars/suv"
+def get_cars_on_page(url, dict):
+  page = requests.get(url)
+  soup = BeautifulSoup(page.content, 'html.parser')
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+  results = soup.find_all(attrs={'class': 'result-tile'})
 
-results = soup.find_all(attrs={'class': 'result-tile'})
+  # printing the raw html results of the get request
+  # for div in results:
+  #   print(div.prettify())
 
-# printing the raw html results of the get request
-# for div in results:
-#   print(div.prettify())
+  # going by the data-qa attribute every field element has
+  # mileage has other crap in it - split after weird character
+  fields = ['make-model', 'trim-mileage', 'price', 'monthly-payment', 'get-it-by']
 
-# going by the data-qa attribute every field element has
-# mileage has other crap in it - split after weird character
-fields = ['make-model', 'trim-mileage', 'price', 'monthly-payment', 'get-it-by']
-data = {}
+  for result in results:
+    car_id = result.find('a').get('href')
 
-for result in results:
-  car_id = result.find('a').get('href')
+    car_data = {'make-model': '', 'mileage': '', 'price': '', 'monthly-payment': '', 'get-it-by': ''}
+    
+    for index, field in enumerate(fields):
+      curr_field_value = result.find(attrs={'data-qa': field})
+      field_text = curr_field_value.get_text()
+      if field == 'trim-mileage':
 
-  car_data = {'make-model': '', 'mileage': '', 'price': '', 'monthly-payment': '', 'get-it-by': ''}
-  
-  for index, field in enumerate(fields):
-    curr_field_value = result.find(attrs={'data-qa': field})
-    field_text = curr_field_value.get_text()
-    if field == 'trim-mileage':
+        # the mileage part of a result tile also has info on the type of car,
+        # so we have to split the string by the middle dot character and append
+        # the car data to the make-model
+        arr = field_text.split(' • ')
+        car_data['make-model'] += ' - ' + arr[0]
+        car_data['mileage'] = arr[1]
 
-      # the mileage part of a result tile also has info on the type of car,
-      # so we have to split the string by the middle dot character and append
-      # the car data to the make-model
-      arr = field_text.split(' • ')
-      car_data['make-model'] += ' - ' + arr[0]
-      car_data['mileage'] = arr[1]
+      elif field == 'get-it-by':
+        car_data[field] = field_text.replace('Get it by ', '')
 
-    elif field == 'get-it-by':
-      car_data[field] = field_text.replace('Get it by ', '')
+      else:
+        car_data[field] = field_text
 
-    else:
-      car_data[field] = field_text
+    dict[car_id] = car_data
 
-  data[car_id] = car_data
+def main():
+  url = "https://www.carvana.com/cars/suv"
+  dict = {}
 
-json_data = json.dumps(data, indent = 4)
+  NUM_PAGES = 10
+  for page in range(1, NUM_PAGES + 1):
+    curr_url = url
+    if page > 1:
+      curr_url += '?page=' + str(page)
 
-# if the car_data folder does not exist, create it
-folder_name = 'car_data/'
-if not os.path.exists(folder_name):
-  os.mkdir(folder_name)
+    get_cars_on_page(curr_url, dict)
 
-now = str(datetime.now())
-filename = now.replace(' ', '_') + '_carvana.json'
-filename = 'car_data/' + filename.replace(':', ';')
-with open(filename, 'w') as output_file:
-  output_file.write(json_data)
+  json_data = json.dumps(dict, indent = 4)
 
-print('Data written to:', filename)
+  # if the car_data folder does not exist, create it
+  folder_name = 'car_data/'
+  if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+
+  now = str(datetime.now())
+  filename = now.replace(' ', '_') + '_carvana.json'
+  filename = 'car_data/' + filename.replace(':', ';')
+  with open(filename, 'w') as output_file:
+    output_file.write(json_data)
+
+  print('Gathered data on', len(dict), 'cars')
+  print('Data written to:', filename)
+
+if __name__ == '__main__':
+  main()
